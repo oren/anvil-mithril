91fa98864373be62e3e5854985e375048f387d5e
Christian Smith <smith@anvil.io>
2015-07-05 16:02:35 -0700

    logout link

--- website/app.js
+++ website/app.js
@@ -3,6 +3,27 @@ var j2c = require('j2c');
 
 var $ = document.querySelector.bind(document);
 var session = require('./components/session.js');
+var callback = require('./components/callback.js');
 
-m.mount($('body'), session);
+
+Anvil.configure({
+  issuer:       'https://connect.anvil.io',
+  client_id:    'a0764ec2-2d1a-4a11-a04f-41df249dafb8',
+  redirect_uri: 'http://connect.anvil.io:3001/callback',
+  //display:      'page',
+  //scope:        'realm'
+});
+
+Anvil.deserialize();
+
+
+//m.mount($('body'), session);
+m.route.mode = 'pathname';
+
+
+m.route(document.body, "/", {
+  '/': session,
+  '/callback': session
+});
+
 
